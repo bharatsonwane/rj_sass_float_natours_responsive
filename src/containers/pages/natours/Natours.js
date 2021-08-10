2b32c3daad87489130c20b841eb5f80111562866
bharatsonwane <bharatrsonwane@gmail.com>
2021-08-10 10:00:24 +0530

    helpr folder structure

--- src/containers/pages/natours/Natours.js
+++ src/containers/pages/natours/Natours.js
@@ -1,5 +1,5 @@
 import React, { Fragment } from 'react'
-import { allClass } from 'src/constants/customModuleClassMethod'
+import { allClass } from 'src/helper/customModuleClassMethod'
 import mdl from "./natours.module.scss"
 
 import Navigation from 'src/components/navbarMenu/Navigation'
